d428199cfcad1f799a36095e680b6b7b6b6599e7
mhsayed2021 <m.hussein@alexu.edu.eg>
2021-12-27 20:54:51 +0300

    updated

--- R/modeMAPE.R
+++ R/modeMAPE.R
@@ -1,31 +1,15 @@
-# Mode of MAPE by 1- Dmax: direct maximization of log(g(x))
-#2- rootF: Root finding of d/dx(log(g(x)))
-modeMAPE<-function(start=0,parm,lower=0,upper,method="Dmax")
-{
+# Mode of MAPE by direct maximization of log(g(x))
+modeMAPE<-function(start=0,parm,lower=0,upper)
+  {
   alpha<-parm["alpha"]
   beta<-parm["beta"]
   rate<-parm["rate"]
-  if (method=="Dmax")
+  objfn <- function(param,x)
     {
-    objfn <- function(param,x)
-      {
-      Logg <- log(alpha)+log(rate)-log(alpha*beta-1)+log(beta)*(1-exp(-rate*x))^2-log(alpha)*exp(-rate*x)-rate*x+log(log(alpha)+2*(1-exp(-rate*x))*log(beta))
-      return(-Logg)
-      }
-    opt <- optim(par = start,fn=objfn, param = parm,method = "Brent",lower = 0,upper = upper)
-    Mode<-opt$par
+    Logg <- log(alpha)+log(rate)-log(alpha*beta-1)+log(beta)*(1-exp(-rate*x))^2-log(alpha)*exp(-rate*x)-rate*x+log(log(alpha)+2*(1-exp(-rate*x))*log(beta))
+    return(-Logg)
     }
-  else if (method=="rootF")
-    {
-    fn<-function (x,param)
-      {
-      CDF<-pexp(x,rate = rate)
-      PDF<-dexp(x,rate)
-      df<--rate^2*exp(-rate*x)
-      dLogg <- 2*CDF*PDF*log(beta)+PDF*log(alpha)+df/PDF
-      dLogg<-dLogg+(2*PDF*log(beta))/(log(alpha)+2*CDF*log(beta))
-      }
-    uni <- uniroot(f=fn, param=parm,lower = 0,upper = upper)
-    Mode<-uni$root
+  opt <- optim(par = start,fn=objfn, param = parm,method = "Brent",lower = lower,upper = upper)
+  opt$par
   }
-  }
+
