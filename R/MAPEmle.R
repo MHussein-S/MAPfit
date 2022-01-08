384ba7fdb323600f4e1601f17db74a9219822f85
mhsayed2021 <m.hussein@alexu.edu.eg>
2022-01-09 02:15:06 +0300

    updated to construct CI

--- R/MAPEmle.R
+++ R/MAPEmle.R
@@ -1,4 +1,3 @@
-#MLE for MAPE with constrOptim
 MAPEmle<-function (data,start)
 {
   #total gradient of log-likelihood function of MAPE
@@ -32,6 +31,33 @@ MAPEmle<-function (data,start)
     LL <- n*log(alpha)+n*log(rate)-n*log(alpha*beta-1)+log(beta)*sum((1-exp(-rate*obs))^2)-log(alpha)*sum(exp(-rate*obs))-rate*sum(obs)+sum(log(log(alpha)+2*(1-exp(-rate*obs))*log(beta)))
     return(-LL)
   }
+  #variance covarince matrix
+  vCmatrix <- function(par, obs)
+  {
+    n<-length(obs)
+    alpha<-par[1]
+    beta<-par[2]
+    lambda<-par[3]
+    domn<-log(alpha)+2*(1-exp(-lambda*obs))*log(beta)
+    val1<-sum(1/domn)
+    val2<-sum(1/domn^2)
+    
+    d2alpha<- -n/alpha^2 + n*beta^2/(alpha*beta-1)^2 + 1/alpha^2*sum(exp(-lambda*obs)) - 1/alpha^2*val2 - 1/alpha*val1
+    
+    d2alpha_beta<- n/(alpha*beta-1)^2 - 2/(alpha*beta)*sum((1-exp(-lambda*obs))/domn^2)
+    
+    d2alpha_lambda<-1/alpha*sum(obs*exp(-lambda*obs)) - 2*log(beta)/alpha*sum(obs*exp(-lambda*obs)/domn^2)
+    
+    d2beta<-n*alpha^2/(alpha*beta-1)^2 - 1/beta^2*sum((1-exp(-lambda*obs))^2) -4/beta^2*sum((1-exp(-lambda*obs))^2/domn^2) - 2/beta^2*sum((1-exp(-lambda*obs))/domn)
+    
+    d2beta_lambda<-2/beta*sum(obs*exp(-lambda*obs)*(1-exp(-lambda*obs))) - 4*log(beta)/beta*sum(obs*exp(-lambda*obs)*(1-exp(-lambda*obs))/domn^2)+2/beta*sum(obs*exp(-lambda*obs)/domn)
+    
+    d2lambda<- -n/lambda^2 + 2*log(beta)*sum(obs^2*exp(-2*lambda*obs)) - 2*log(beta)*sum(obs^2*exp(-lambda*obs)*(1-exp(-lambda*obs))) - 2*log(beta)*sum(obs^2*exp(-lambda*obs)) - 4*(log(beta))^2*sum(obs^2*exp(-2*lambda*obs)/domn^2) - 2*log(beta)*sum(obs^2*exp(-lambda*obs)/domn)
+    
+    I<-matrix(c(-d2alpha,-d2alpha_beta,-d2alpha_lambda,-d2alpha_beta,-d2beta,-d2beta_lambda,-d2alpha_lambda,-d2beta_lambda,-d2lambda),nrow = 3,ncol = 3,byrow = T)
+    solve(I)
+    
+  }
   npar <- 3
   Mat <- diag(3)
   colnames(Mat) <- c("alpha","beta","rate")
@@ -39,23 +65,29 @@ MAPEmle<-function (data,start)
   initconstr <- Mat %*% start - c(1,1,0)
   if (any(initconstr < 0))
     stop("Starting values must be in the feasible region.")
-  opt <- stats::constrOptim(theta=start,f=objfn,grad = grlnlMAP, obs = data , ui=Mat,ci = c(1,1,0))
+  opt <- constrOptim(theta=start,f=objfn,grad = grlnlMAP, obs = data , ui=Mat,ci = c(1,1,0),hessian = T)
   if (is.null(names(opt$par)))
     names(opt$par) <- c("alpha","beta","rate")
+  #---
+  A<-vCmatrix(opt$par,data)
+  prop_sigma<-sqrt(diag(A))
+  upper<-opt$par+qnorm(0.975)*prop_sigma
+  lower<-opt$par+qnorm(0.025)*prop_sigma
+  #---
   loglik <- -opt$value
   k<-npar
   n<-length(data)
   AIC<--2*loglik+2*k
   BIC<- -2*loglik+ k*log(n)
   HQIC<- -2*loglik+2*k*log(log(n))
-  res=cbind(opt$par)
-  colnames(res)=c("MLE")
+  res=cbind(opt$par,prop_sigma,lower,upper)
+  colnames(res)=c("MLE","Std. Err", "Inf. 95% CI","Sup. 95% CI")
   res1=cbind(AIC,BIC, HQIC, opt$value)
   colnames(res1)=c("AIC","BIC","HQIC", "-log(Likelihood)")
   rownames(res1)=c("")
-  yemp<-stats::ecdf(data)
+  yemp<-ecdf(data)
   ytheo<-pMAP(sort(data),par=opt$par,'exp' )
-  KS<-stats::ks.test(yemp(data),ytheo)
+  KS<-ks.test(yemp(data),ytheo)
   res2=cbind(KS$statistic,KS$p.value)
   colnames(res2)=c("KS Statistic","KS p-value")
   rownames(res2)=c("")
@@ -64,8 +96,3 @@ MAPEmle<-function (data,start)
   rownames(res3)=c("")
   list("Estimates"=res,"Measures"=res1,"Kolmogorov-Smirnov Test"=res2,"Convergence Status"=res3)
 }
-
-
-
-
-
